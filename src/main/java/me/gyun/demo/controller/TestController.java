58bd72998865c6c41062d33de9e4c89657e0cf5a
Gyunny <wjdrbs966@naver.com>
2021-03-05 17:55:31 +0900

    Jenkins Test2

--- src/main/java/me/gyun/demo/controller/TestController.java
+++ src/main/java/me/gyun/demo/controller/TestController.java
@@ -10,4 +10,9 @@ public class TestController {
     public String test() {
         return "test";
     }
+
+    @GetMapping("/test")
+    public String jenkinsTest() {
+        return "jenkins";
+    }
 }
