045ff0e5d04503702dcc4c66bb7283c3d4fe099b
Gyunny <wjdrbs966@naver.com>
2021-03-07 17:03:36 +0900

    기도 중

--- src/main/java/me/gyun/demo/controller/TestController.java
+++ src/main/java/me/gyun/demo/controller/TestController.java
@@ -1,9 +1,10 @@
 package me.gyun.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+/**
+ * created by jg 2021/03/07
+ */
 public class TestController {
 
     @GetMapping("/")
