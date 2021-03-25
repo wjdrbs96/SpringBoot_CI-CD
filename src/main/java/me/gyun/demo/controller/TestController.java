5620f92edfc7b88b7b8f3ae039993e22aa390fed
wjdrbs96 <wjdrbs966@naver.com>
2021-03-25 11:12:29 +0900

    어노테이션 add

--- src/main/java/me/gyun/demo/controller/TestController.java
+++ src/main/java/me/gyun/demo/controller/TestController.java
@@ -1,10 +1,12 @@
 package me.gyun.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * created by jg 2021/03/07 bb
  */
+@RestController
 public class TestController {
 
     @GetMapping("/lee")
