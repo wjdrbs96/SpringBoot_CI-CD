c7e81e7aabeff3194a8afac6613958741b5f021e
wjdrbs96 <wjdrbs966@naver.com>
2021-03-25 11:38:34 +0900

    Last Test

--- src/main/java/me/gyun/demo/controller/TestController.java
+++ src/main/java/me/gyun/demo/controller/TestController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/lee")
+    @GetMapping("/")
     public String test() {
         return "TEst";
     }
