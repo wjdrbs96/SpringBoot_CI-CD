85181a96bf8a2934db6432993daa92727720ef95
wjdrbs96 <wjdrbs966@naver.com>
2021-03-25 13:24:24 +0900

    후

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/app/step2/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/app/step2/zip/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
 echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/deploy.log
 
 echo "> build 파일 복사" >> /home/ec2-user/deploy.log
-DEPLOY_PATH=/home/ec2-user/app/step/
+DEPLOY_PATH=/home/ec2-user/app/step/zip
 cp $BUILD_JAR $DEPLOY_PATH
 
 echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
