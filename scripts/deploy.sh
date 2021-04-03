d8f24498ef56bc9060454e01c5e001fd371d92b3
wjdrbs96 <wjdrbs966@naver.com>
2021-04-03 18:09:11 +0900

    deploy update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,15 @@ echo "> build 파일 복사" >> /home/ec2-user/deploy.log
 DEPLOY_PATH=/home/ec2-user/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
-CURRENT_PID=$(pgrep -f $JAR_NAME)
+echo "> 현재 실행 중인 Docker 컨테이너 pid 확인" >> /home/ec2-user/deploy.log
+CURRENT_PID=$(sudo docker container ls -q)
 
 if [ -z $CURRENT_PID ]
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> sudo docker stop $CURRENT_PID"
+  sudo docker stop $CURRENT_PID
   sleep 5
 fi
 
