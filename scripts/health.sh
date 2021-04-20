fa22685aa427ceb243efcb0c7f24191a08e7c336
wjdrbs96 <wjdrbs966@naver.com>
2021-04-20 23:47:55 +0900

    test

--- scripts/health.sh
+++ scripts/health.sh
@@ -7,31 +7,31 @@ IDLE_PORT=$(find_idle_port)
 
 echo "> Health Check Start!"
 echo "> IDLE_PORT: $IDLE_PORT"
-echo "> curl -s http://3.36.209.141:$IDLE_PORT"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
 sleep 10
 
 for RETRY_COUNT in {1..10}
 do
-  RESPONSE=$(curl -s http://3.36.209.141:${IDLE_PORT})
+  RESPONSE=$(curl -s http://3.36.209.141/:${IDLE_PORT})
   UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
 
   if [ ${UP_COUNT} -ge 1 ]
-  then
-    echo "> Health check 성공"
-    swich_proxy
-    break
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
   if [ ${RETRY_COUNT} -eq 10 ]
   then
-    echo "> Health check 실패."
-    echo "> 엔직엑스에 연결하지 않고 배포를 종료합니다."
+    echo "> Health check 실패. "
+    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
     exit 1
   fi
 
-  echo "> Health check 연결 실패 재시도.."
+  echo "> Health check 연결 실패. 재시도..."
   sleep 10
 done
--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,14 +1,12 @@
-#! /usr/bin/env bash
+function find_idle_profile()
+{
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
 
-function find_idle_profile() {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://3.36.209.141)
-
-    if [ ${RESPONSE_CODE} -ge 400 ]
-
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
-      CURRENT_PROFILE=real2
+        CURRENT_PROFILE=real2
     else
-      CURRENT_PROFILE=$(curl -s http://3.36.209.141)
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
@@ -21,7 +19,9 @@ function find_idle_profile() {
     echo "${IDLE_PROFILE}"
 }
 
-function find_idle_port() {
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
     IDLE_PROFILE=$(find_idle_profile)
 
     if [ ${IDLE_PROFILE} == real1 ]
