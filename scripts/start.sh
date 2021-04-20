168c5a3779b1529b371e8c230b66b5a94f9b9232
wjdrbs96 <wjdrbs966@naver.com>
2021-04-20 23:27:32 +0900

    test

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,25 +4,25 @@ source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 
-PROJENCT_NAME=SpringBoot_CI-CD
-
 echo "> Build 파일 복사"
-echo "> cp $REPOSITORY/zip/*jar ${REPOSITORY}/"
+echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*jar $REPOSITORY/
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name: $JAR_Name"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> JAR_Name 에 실행 권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
-chmod *x $JAR_NAME
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
 
 IDLE_PROFILE=$(find_idle_profile)
 
 echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
-
 nohup java -jar \
     -Dspring.config.location=classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
