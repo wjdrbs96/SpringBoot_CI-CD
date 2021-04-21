02812e0ea6919da4e7086cce7ebab8fa19c501bc
wjdrbs96 <wjdrbs966@naver.com>
2021-04-21 09:36:17 +0900

    Test ~~

--- src/main/java/me/gyun/demo/controller/TestController.java
+++ src/main/java/me/gyun/demo/controller/TestController.java
@@ -1,17 +1,32 @@
 package me.gyun.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * created by jg 2021/03/07
  */
+@RequiredArgsConstructor
 @RestController
 public class TestController {
 
+    private final Environment env;
+
     @GetMapping("/")
     public String gyunny() {
-        return "타겟그룹 A ㅎㅎ";
+        List<String> profile = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2", "real2");
+        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
+
+        return profile.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
 
     @GetMapping("/test")
